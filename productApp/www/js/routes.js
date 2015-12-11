07ef0b388fa226befcdb123caeb63381b49cece4
Alejandro Romero <alejandro.romero@dmkcolomiba.com>
2015-12-11 14:32:23 -0500

    Update productApp :bowtie:

--- productApp/www/js/routes.js
+++ productApp/www/js/routes.js
@@ -7,17 +7,20 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-      
+
+
     .state('menu', {
       url: '/menu',
       abstract:true,
-      templateUrl: 'templates/menu.html'
+      templateUrl: 'templates/menu.html',
+      data: {
+          requireLogin: true
+      }
     })
-      
-    
-      
-        
+
+
+
+
     .state('menu.home', {
       url: '/home',
       views: {
@@ -25,121 +28,134 @@ angular.module('app.routes', [])
           templateUrl: 'templates/home.html',
           controller: 'homeCtrl'
         }
+      },
+      data: {
+        requireLogin: true
       }
     })
-        
-      
-    
-      
-        
+
+
     .state('options', {
       url: '/options',
       templateUrl: 'templates/options.html',
-      controller: 'optionsCtrl'
+      controller: 'optionsCtrl',
+      data: {
+        requireLogin: false
+      }
     })
-        
-      
-    
-      
-        
+
+
     .state('signIn', {
       url: '/sign-in',
       templateUrl: 'templates/signIn.html',
-      controller: 'signInCtrl'
+      controller: 'signInCtrl',
+      data: {
+        requireLogin: false
+      }
     })
-        
-      
-    
-      
-        
+
+
+
+
+
     .state('signUp', {
       url: '/sign-up',
       templateUrl: 'templates/signUp.html',
-      controller: 'signUpCtrl'
+      controller: 'signUpCtrl',
+      data: {
+        requireLogin: false
+      }
     })
-        
-      
-    
-      
-        
+
+
+
+
+
     .state('productDetail', {
-      url: '/product-detail',
+      url: '/product-detail/:id',
       templateUrl: 'templates/productDetail.html',
-      controller: 'productDetailCtrl'
+      controller: 'productDetailCtrl',
+      data: {
+        requireLogin: true
+      }
     })
-        
-      
-    
-      
-        
+
+
     .state('productEdit', {
-      url: '/product-edit',
+      url: '/product-edit/:id',
       templateUrl: 'templates/productEdit.html',
-      controller: 'productEditCtrl'
+      controller: 'productEditCtrl',
+      data: {
+        requireLogin: true
+      }
     })
-        
-      
-    
-      
-        
+
+
+
     .state('productCreate', {
       url: '/product-create',
       templateUrl: 'templates/productCreate.html',
-      controller: 'productCreateCtrl'
+      controller: 'productCreateCtrl',
+      data: {
+        requireLogin: true
+      }
     })
-        
-      
-    
-      
-        
+
+
     .state('menu.profile', {
       url: '/profile',
       views: {
         'side-menu21': {
           templateUrl: 'templates/profile.html',
-          controller: 'profileCtrl'
+          controller: 'profileCtrl',
+          data: {
+            requireLogin: true
+          }
         }
       }
     })
-        
-      
-    
-      
-        
+
+
     .state('profileEdit', {
       url: '/profile-edit',
       templateUrl: 'templates/profileEdit.html',
-      controller: 'profileEditCtrl'
+      controller: 'profileEditCtrl',
+      data: {
+        requireLogin: true
+      }
     })
-        
-      
-    
-      
-        
+
+
     .state('forgotPassword', {
       url: '/forgot-password',
       templateUrl: 'templates/forgotPassword.html',
-      controller: 'forgotPasswordCtrl'
+      controller: 'forgotPasswordCtrl',
+      data: {
+        requireLogin: true
+      }
     })
-        
-      
-    
-      
-        
+
+
+
     .state('menu.signOut', {
       url: '/sign-out',
       views: {
         'side-menu21': {
           templateUrl: 'templates/signOut.html',
-          controller: 'signOutCtrl'
+          controller: 'signOutCtrl',
+          data: {
+            requireLogin: false
+          }
         }
       }
-    })
-        
-      
-    ;
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/menu/home');
+  // $urlRouterProvider.otherwise('/menu/home');
+
+  $urlRouterProvider.otherwise(function($injector, $location){
+    var $state = $injector.get("$state");
+    $state.go('menu.home');
+  });
 
 });
